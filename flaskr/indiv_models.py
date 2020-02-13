c30905127e29343ed52315a71d337b6b33b1e16a
Chien  Tan <chien.tan16@imperial.ac.uk>
2020-02-13 23:04:55 +0000

    skel code

---
+++ flaskr/indiv_models.py
@@ -0,0 +1,4 @@
+class PositiveRatioModel:
+    model = 0
+
+
--- flaskr/recommender.py
+++ flaskr/recommender.py
@@ -2,6 +2,7 @@ import requests
 from flask import jsonify
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from reactionrnn import reactionrnn
+from . import indiv_models
 
 url = ('https://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=cde202936bf948eeb49767c5b3495493')
 
@@ -38,4 +39,20 @@ def selectorVader(articles):
     # print([ x[1] for x in sorted_articles_scores ])
 
     return sorted_articles[:10]
+
+def selectorIndiv(articles):
+    indivModel = 
+    scores = []
+
+    for article in articles:
+        score = indivModel.polarity_scores(article['title'])
+        scores.append(score['compound'])
+    articles_scores = list(zip(articles, scores))
+    sorted_articles_scores = sorted(articles_scores, key=lambda x : x[1], reverse=True)
+    
+    sorted_articles = [ x[0] for x in sorted_articles_scores ]
+    print([ x[1] for x in sorted_articles_scores ])
+
+    return sorted_articles[:10]
+    
 
