ffe7df8e8b9dc021bf73905b56e81616df3cd6cc
Chien  Tan <chien.tan16@imperial.ac.uk>
2020-02-16 20:17:04 +0000

    change req

--- flaskr/indiv_models.py
+++ flaskr/indiv_models.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import *
 import pandas as pd
-import numpy as np
+# import numpy as np
 from . import util
 
 
--- flaskr/recommender.py
+++ flaskr/recommender.py
@@ -1,7 +1,7 @@
 import requests
 from flask import jsonify
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from reactionrnn import reactionrnn
+# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+# from reactionrnn import reactionrnn
 from . import indiv_models
 
 url = ('https://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=cde202936bf948eeb49767c5b3495493')
@@ -20,33 +20,33 @@ class Recommender:
 
         return self.selectorIndiv(articles)
 
-    def selectorReact(self, articles):
-        react = reactionrnn()
-        selected = []
+    # def selectorReact(self, articles):
+    #     react = reactionrnn()
+    #     selected = []
 
-        for article in articles:
-            labels = react.predict_label(article['content'])
+    #     for article in articles:
+    #         labels = react.predict_label(article['content'])
 
-            if 'love' in labels:
-                selected.append(article)
+    #         if 'love' in labels:
+    #             selected.append(article)
 
-        return selected
+    #     return selected
 
-    def selectorVader(self, articles):
-        analyser = SentimentIntensityAnalyzer()
-        scores = []
+    # def selectorVader(self, articles):
+    #     analyser = SentimentIntensityAnalyzer()
+    #     scores = []
 
-        for article in articles:
-            score = analyser.polarity_scores(article['title'])
-            scores.append(score['compound'])
-        articles_scores = list(zip(articles, scores))
-        sorted_articles_scores = sorted(
-            articles_scores, key=lambda x: x[1], reverse=True)
+    #     for article in articles:
+    #         score = analyser.polarity_scores(article['title'])
+    #         scores.append(score['compound'])
+    #     articles_scores = list(zip(articles, scores))
+    #     sorted_articles_scores = sorted(
+    #         articles_scores, key=lambda x: x[1], reverse=True)
 
-        sorted_articles = [x[0] for x in sorted_articles_scores]
-        # print([ x[1] for x in sorted_articles_scores ])
+    #     sorted_articles = [x[0] for x in sorted_articles_scores]
+    #     # print([ x[1] for x in sorted_articles_scores ])
 
-        return sorted_articles[:10]
+    #     return sorted_articles[:10]
 
     def selectorIndiv(self, articles):
         # model = indiv_models.PositiveRatioModel()
